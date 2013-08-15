1fd6979a96f00f98f4671fe04c77c780aff9abfe
kaiser-home <renkaikaiser@163.com>
2013-08-15 21:37:23 +0800

    EmberZNet/EM35x/app/Driver 1. CHG: initial proposal is followed with datasheet description.

--- firmware/EmberZNet/EM35x/app/Driver/TWI.c
+++ firmware/EmberZNet/EM35x/app/Driver/TWI.c
@@ -31,10 +31,6 @@ int32u twi_status;
  *----------------------------------------------------------------------------*/
 void twi_init(void)
 {
-   halGpioConfig(TWI_SDA(A, 1), GPIOCFG_OUT_ALT_OD ); //SDA PA1
-   halGpioConfig(TWI_SCL(A, 2), GPIOCFG_OUT_ALT_OD ); //SCL PA2
-
-   SC2_MODE = SC2_MODE_I2C;
    /*
     * LIN: 14  EXP: 3  100kbps
     * LIN: 15  EXP: 1  375kbps
@@ -42,33 +38,40 @@ void twi_init(void)
     */
    SC2_RATELIN = 14; //100kbps
    SC2_RATEEXP = 3;
+   
+   /*
+    * SDA and SCL MUST BE open-drain.
+    */
+   halGpioConfig(TWI_SDA(A, 1), GPIOCFG_OUT_ALT_OD ); //SDA PA1
+   halGpioConfig(TWI_SCL(A, 2), GPIOCFG_OUT_ALT_OD ); //SCL PA2
 
-   SC2_INTMODE = SC_TXIDLELEVEL | SC_TXFREELEVEL | SC_RXVALLEVEL;
-   INT_SC2CFG = INT_SCRXFIN | INT_SCTXFIN | INT_SCCMDFIN | INT_SCNAK;
-   INT_CFGSET = INT_SC2;
+   /*
+    * Enable INT
+    */
+   //SC2_INTMODE = SC_TXIDLELEVEL | SC_TXFREELEVEL | SC_RXVALLEVEL;
+   //INT_SC2CFG = INT_SCRXFIN | INT_SCTXFIN | INT_SCCMDFIN | INT_SCNAK;
+   //INT_CFGSET = INT_SC2;
+   
+   /*
+    * Enable TWI mode. 
+    */
+   SC2_MODE = SC2_MODE_I2C;
 }
 
 void twi_wr(int32u addr, int8u data)
 {
-   //twi_status = 0;
+   //twi_status = SC2_TWISTAT;
    SC2_TWICTRL1 = SC_TWISTART;   //start bit
 
-   //while( !( twi_status & SC_TWICMDFIN) );  //wait for S/P complete
+   while( !( SC2_TWISTAT & SC_TWICMDFIN) );  //wait for S/P complete
 
-   //if (len ){
-   //   SC2_TWICTRL1 = SC_TWISEND;   //start send
-   //}
- 	//while(len){
-      SC2_DATA = addr << 1;
-	  SC2_TWICTRL1 = SC_TWISEND;   //start send
-      //len--;
-      while( !( twi_status & SC_TWITXFIN ) );
+   SC2_DATA = addr << 1;
+   SC2_TWICTRL1 = SC_TWISEND;   //start send
+   while( !( SC2_TWISTAT & SC_TWITXFIN ) );
 
-	  SC2_DATA = data;
-	  SC2_TWICTRL1 = SC_TWISEND;   //start send
-      //len--;
-      while( !( twi_status & SC_TWITXFIN ) );
-   //}
+	SC2_DATA = data;
+	SC2_TWICTRL1 = SC_TWISEND;   //start send
+   while( !( SC2_TWISTAT & SC_TWITXFIN ) );
 
    SC2_TWICTRL1 = SC_TWISTOP;   //start stop
 }
